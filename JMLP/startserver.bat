3ae5a9d2f2ead26e5b9a91895977d818047e7cbf
colindoug <colindoug@9ef82de4-26a0-41fd-854c-22477ec5be06>
2009-03-29 22:57:31 +0000

    git-svn-id: https://masslogprocess.svn.sourceforge.net/svnroot/masslogprocess@7 9ef82de4-26a0-41fd-854c-22477ec5be06

--- JMLP/startserver.bat
+++ JMLP/startserver.bat
@@ -1,3 +1,3 @@
 
-java -classpath ./libs/OROMatcher1.0.jar;./libs/jppf-client.jar;./libs/jppf-common.jar;./libs/jppf-common-node.jar;./libs/commons-logging-1.1.jar;./logpro.jar com.mlp.syslogd.SyslogD
+java -classpath ./libs/OROMatcher1.0.jar;./libs/jppf-client.jar;./libs/jppf-common.jar;./libs/jppf-common-node.jar;./libs/commons-logging-1.1.jar;./jmasslogpro.jar com.mlp.syslogd.SyslogD
 
